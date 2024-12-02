e0bef039114f304ee1e21ff50c31b678149e4e78
Om Patel <okmpatel@gmail.com>
2024-12-02 07:27:48 -0500

    Change home UI
    made some change to home page and created css file for CrimeCard and moved them to their own folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <header style={{ padding: "10px", background: "#333", color: "#fff" }}>
-          <nav style={{ display: "flex", gap: "20px" }}>
+        <header>
+          <nav>
             <Link href="/">Home</Link>
             <Link href="/alldata">All Crime</Link>
             <Link href="/vehicledata">Vehicle Crime</Link>
